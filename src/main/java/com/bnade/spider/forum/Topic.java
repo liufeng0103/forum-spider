7d93259c18458685e339abbca4b439af16d801b3
Feng Liu <liufeng0103@163.com>
2018-06-14 19:27:11 +0800

    Topic接口添加方法

--- src/main/java/com/bnade/spider/forum/Topic.java
+++ src/main/java/com/bnade/spider/forum/Topic.java
@@ -16,18 +16,31 @@ public interface Topic {
 	int getId();
 
 	/**
-	 * 设置帖子id
-	 * 
-	 * @param id 帖子id
-	 */
-	void setId(int id);
-
-	/**
 	 * 获取帖子标题
 	 * 
 	 * @return 帖子标题
 	 */
 	String getTitle();
+
+	/**
+	 * 获取帖子发布者用户名
+	 * @return 帖子发布者用户名
+	 */
+	String getAuthor();
+
+	/**
+	 * 获取帖子链接
+	 * 
+	 * @return 帖子链接
+	 */
+	String getLink();
+
+	/**
+	 * 设置帖子id
+	 * 
+	 * @param id 帖子id
+	 */
+	void setId(int id);
 
 	/**
 	 * 设置帖子标题
@@ -37,15 +50,15 @@ public interface Topic {
 	void setTitle(String title);
 
 	/**
-	 * 获取帖子发布者用户名
-	 * @return 帖子发布者用户名
-	 */
-	String getAuthor();
-
-	/**
 	 * 设置帖子发布者用户名
 	 * @param author 帖子发布者用户名
 	 */
 	void setAuthor(String author);
-
+	
+	/**
+	 * 设置帖子链接
+	 * @param link 帖子链接
+	 */
+	void setLink(String link);
+	
 }
